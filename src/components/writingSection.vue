80764dea04e15292cffc40fe359809af9c443afd
AayushPokharel <40122245+AayushPokharel@users.noreply.github.com>
2024-12-08 23:50:26 +0545

    done

--- src/components/writingSection.vue
+++ src/components/writingSection.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue'
-import { useRoute } from 'vue-router'
 
 const articles = ref([])
 
 onMounted(async () => {
-  const response = await fetch('../../articles/articles.json')
+  const response = await fetch('/articles/articles.json')
   articles.value = await response.json()
 })
 
@@ -19,7 +18,7 @@ const reversedArticlesList = computed(() => {
     <h1>Articles</h1>
     <ul>
       <li v-for="article in reversedArticlesList" :key="article.file">
-        <RouterLink :to="`${article.file}`">{{ article.title }}</RouterLink>
+        <RouterLink :to="`${article.file.replace('.md','')}`">{{ article.title }}</RouterLink>
       </li>
     </ul>
   </div>
