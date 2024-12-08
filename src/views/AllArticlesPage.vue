a6bdc0beadb1f7df7ee647a1935365e9f64fad63
swapnil-up <swapnilupadhyaykharel@gmail.com>
2024-12-08 15:45:49 +0545

    Updates

--- src/views/AllArticlesPage.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import writingSection from '@/components/writingSection.vue'
-</script>
-
-<template>
-  <div><p>Here's all of my writings</p></div>
-  <writingSection />
-</template>
--- src/views/HomePage.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <p>
-    Hey there, I'm Swapnil. You can call me swap. The site's currently under construction. Be
-    careful of potholes while you're exploring up in these old parts.
-  </p>
-</template>
