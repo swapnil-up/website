7206d4e3005d88502c93b67c27d4c038b419a9eb
swapnil-up <swapnilupadhyaykharel@gmail.com>
2025-02-18 18:11:55 +0545

    readding index too

--- public/index.php
+++ public/index.php
@@ -1,60 +1,17 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell
- */
-
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is In Maintenance Mode
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance mode, we will return a response
-| indicating that maintenance is currently happening so that all users
-| will see the same message. Otherwise, we will boot the application.
-|
-*/
+define('LARAVEL_START', microtime(true));
 
-if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
-    require __DIR__ . '/../storage/framework/maintenance.php';
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Prepare The Application
-|--------------------------------------------------------------------------
-|
-| We need to prepare the application for serving by bootstrapping the
-| framework and getting the router ready for incoming requests. The
-| $app variable will be used to make the application instance.
-|
-*/
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application set up, we can handle the incoming request
-| using the HTTP kernel. Then, we will send the response back to the
-| browser, allowing the user's request to be fulfilled.
-|
-*/
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__ . '/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
